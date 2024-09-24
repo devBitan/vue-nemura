eeea6cba352d29e2401d5b47ff984f233c648d1f
devBitan <jquiramadev@outlook.com>
2024-09-24 09:50:10 -0500

    feat:dev dependencies sass

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import './assets/main.scss'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
 
 import App from './App.vue'
-import router from './router'
+import router from './router/index';
 
 const app = createApp(App)
 
-app.use(createPinia())
+// Crea la instancia de Pinia
+const pinia = createPinia();
+// Usa el plugin de persistencia
+pinia.use(piniaPluginPersistedstate);
+
+// Usa Pinia y el router en la aplicación
+app.use(pinia);
 app.use(router)
 
 app.mount('#app')
+
